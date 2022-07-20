f571eb486e1281c2c6e4b666a28ded7388491446
roei.levi <roei.levi@accenture.com>
2022-07-20 17:30:06 +0300

    feat(samples): add more samples

--- src/samples/basic.py
+++ src/samples/basic.py
@@ -2,6 +2,8 @@
 from typing import Any, Dict, List
 from cymple.builder import QueryBuilder
 
+
+
 def get_all_nodes_by_label(labels: List[str], node_name: str = 'n'):
     return (QueryBuilder()
             .match()
@@ -9,12 +11,14 @@ def get_all_nodes_by_label(labels: List[str], node_name: str = 'n'):
             .return_single(ref_name=node_name)
             )
 
-def get_all_nodes_by_label_and_property(labels: List[str], properties: Dict[str, Any], node_name: str = 'n'):
+
+def get_all_nodes_by_label_and_properties(labels: List[str], properties: Dict[str, Any], node_name: str = 'n'):
     return (QueryBuilder()
             .match()
             .node(labels=labels, ref_name=node_name, properties=properties)
             .return_single(ref_name=node_name)
             )
+
 
 def get_all_paths(src_node_labels: List[str], dst_node_labels: List[str], relationship_type: str, path_name: str = 'p'):
     return (QueryBuilder()
@@ -27,11 +31,65 @@ def get_all_paths(src_node_labels: List[str], dst_node_labels: List[str], relati
             .return_single(ref_name=path_name)
             )
 
+
 def get_all_nodes_related_to_nodes(src_node_labels: List[str], dst_node_labels: List[str], relationship_type: str, dst_node_name: str = 'd'):
     return (QueryBuilder()
             .match()
             .node(labels=src_node_labels)
             .related(label=relationship_type)
-            .node(labels=dst_node_labels, ref_name = dst_node_name)
+            .node(labels=dst_node_labels, ref_name=dst_node_name)
             .return_single(ref_name=dst_node_name)
+            )
+
+
+def get_all_nodes_related_by_fixed_num_of_hops(src_node_labels: List[str], dst_node_labels: List[str], num_hops: int, dst_node_name: str = 'd'):
+    return (QueryBuilder()
+            .match()
+            .node(labels=src_node_labels)
+            .related_variable_len(min_hops=num_hops, max_hops=num_hops)
+            .node(labels=dst_node_labels, ref_name=dst_node_name)
+            .return_single(ref_name=dst_node_name)
+            )
+
+
+def get_all_nodes_related_by_varying_num_of_hops(src_node_labels: List[str], dst_node_labels: List[str], min_hops: int, max_hops: int, dst_node_name: str = 'd'):
+    return (QueryBuilder()
+            .match()
+            .node(labels=src_node_labels)
+            .related_variable_len(min_hops=min_hops, max_hops=max_hops)
+            .node(labels=dst_node_labels, ref_name=dst_node_name)
+            .return_single(ref_name=dst_node_name)
+            )
+
+
+def get_nodes_with_pagination(node_labels: List[str], node_name: str = 'n', offset: int = 0, limit: int = -1):
+    raise NotImplementedError()
+
+
+def create_node(node_labels: List[str]):
+    raise NotImplementedError()
+
+
+def merge_node(node_labels: List[str], properties: Dict[str, Any] = None):
+    return (QueryBuilder()
+            .merge()
+            .node(labels=node_labels, properties=properties)
+            )
+
+
+def create_relationship(relationship_type: str):
+    raise NotImplementedError()
+
+
+def merge_relationship(relationship_type: str, properties: Dict[str, Any] = None, src_node_labels: List[str] = None, 
+                        dst_node_labels: List[str] = None, src_node_properties: Dict[str, Any] = None, dst_node_properties: Dict[str, Any] = None):
+    return (QueryBuilder()
+            .match()
+            .node(labels=src_node_labels, properties=src_node_properties, ref_name='src')
+            .match()
+            .node(labels=dst_node_labels, properties=dst_node_properties, ref_name='dst')
+            .merge()
+            .node(ref_name='src')
+            .related_to(label=relationship_type, properties=properties)
+            .node(ref_name='dst')
             )
--- tests/unit/test_samples.py
+++ tests/unit/test_samples.py
@@ -3,25 +3,44 @@ from src.samples import basic
 
 
 rendered = {
+    # Read Operations #
     basic.get_all_nodes_by_label.__name__:
         basic.get_all_nodes_by_label(labels='Movie', node_name='n'),
-    basic.get_all_nodes_by_label_and_property.__name__:
-        basic.get_all_nodes_by_label_and_property(labels='Movie', properties={'name': 'The Matrix'}, node_name='n'),
+    basic.get_all_nodes_by_label_and_properties.__name__:
+        basic.get_all_nodes_by_label_and_properties(labels='Movie', properties={'name': 'The Matrix'}, node_name='n'),
     basic.get_all_paths.__name__:
         basic.get_all_paths(src_node_labels='Movie', dst_node_labels='Director', relationship_type='has_directed', path_name='path'),
     basic.get_all_nodes_related_to_nodes.__name__:
-        basic.get_all_nodes_related_to_nodes(src_node_labels='Movie', dst_node_labels='Director', relationship_type='has_directed', dst_node_name='director')
+        basic.get_all_nodes_related_to_nodes(src_node_labels='Movie', dst_node_labels='Director', relationship_type='has_directed', dst_node_name='director'),
+    basic.get_all_nodes_related_by_fixed_num_of_hops.__name__:
+        basic.get_all_nodes_related_by_fixed_num_of_hops(src_node_labels='Movie', dst_node_labels='Director', num_hops=2, dst_node_name='director'),
+    basic.get_all_nodes_related_by_varying_num_of_hops.__name__:
+        basic.get_all_nodes_related_by_varying_num_of_hops(src_node_labels='Movie', dst_node_labels='Director', min_hops=1, max_hops=3, dst_node_name='director'),
+    # Write Operations #
+    basic.merge_node.__name__:
+        basic.merge_node(node_labels='Movie', properties={'name': 'The Matrix Revolutions'}),
+    basic.merge_relationship.__name__:
+        basic.merge_relationship(relationship_type='has_sequel', src_node_labels='Movie', src_node_properties={'name': 'The Matrix'}, 
+                                dst_node_labels='Movie', dst_node_properties={'name': 'The Matrix Revolutions'}),
 }
 
 expected = {
     basic.get_all_nodes_by_label.__name__: 
         'MATCH (n: Movie) RETURN n',
-    basic.get_all_nodes_by_label_and_property.__name__: 
+    basic.get_all_nodes_by_label_and_properties.__name__: 
         'MATCH (n: Movie {name : "The Matrix"}) RETURN n',
     basic.get_all_paths.__name__: 
         'MATCH path = ( (: Movie)-[: has_directed]-(: Director) ) RETURN path',
     basic.get_all_nodes_related_to_nodes.__name__: 
-        'MATCH (: Movie)-[: has_directed]-(director: Director) RETURN director'
+        'MATCH (: Movie)-[: has_directed]-(director: Director) RETURN director',
+    basic.get_all_nodes_related_by_fixed_num_of_hops.__name__: 
+        'MATCH (: Movie)-[*2]-(director: Director) RETURN director',
+    basic.get_all_nodes_related_by_varying_num_of_hops.__name__: 
+        'MATCH (: Movie)-[*1..3]-(director: Director) RETURN director',
+    basic.merge_node.__name__: 
+        'MERGE (: Movie {name : "The Matrix Revolutions"})',
+    basic.merge_relationship.__name__: 
+        'MATCH (src: Movie {name : "The Matrix"}) MATCH (dst: Movie {name : "The Matrix Revolutions"}) MERGE (src)-[: has_sequel]->(dst)',
 }
 
 
