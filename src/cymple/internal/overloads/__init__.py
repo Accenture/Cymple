08f0f8087cb13b6f9205e55ccf7cd69b46b36ef0
yaelzamir <yaelzamir@accenture.com>
2023-02-14 17:21:36 +0200

    Add some successors to case when and delete + fix overloads init

--- src/cymple/internal/overloads/__init__.py
+++ src/cymple/internal/overloads/__init__.py
@@ -9,7 +9,7 @@ overloads_module = sys.modules[__name__]
 for module_name in os.listdir(os.path.dirname(__file__)):
     if module_name == '__init__.py' or module_name[-3:] != '.py':
         continue
-    module = importlib.import_module(f'cymple.internal.overloads.{module_name[:-3]}')
+    module = importlib.import_module(f'.{module_name[:-3]}', __name__)
     funcs = inspect.getmembers(module, inspect.isfunction)
     for name, func in funcs:
         setattr(overloads_module, name, func)
