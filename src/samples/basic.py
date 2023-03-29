68236ef1b6a0eb7ad69a579016b18b00db2bc12f
roei.levi <roei.levi@accenture.com>
2023-03-29 18:50:48 +0300

    fix: node labels type hint

--- src/samples/basic.py
+++ src/samples/basic.py
@@ -1,10 +1,10 @@
 
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 from cymple.builder import QueryBuilder
 
 
 
-def get_all_nodes_by_label(labels: List[str] | str, node_name: str = 'n'):
+def get_all_nodes_by_label(labels: Union[List[str], str], node_name: str = 'n'):
     return (QueryBuilder()
             .match()
             .node(labels=labels, ref_name=node_name)
@@ -12,7 +12,7 @@ def get_all_nodes_by_label(labels: List[str] | str, node_name: str = 'n'):
             )
 
 
-def get_all_nodes_by_label_and_properties(labels: List[str] | str, properties: Dict[str, Any], node_name: str = 'n'):
+def get_all_nodes_by_label_and_properties(labels: Union[List[str], str], properties: Dict[str, Any], node_name: str = 'n'):
     return (QueryBuilder()
             .match()
             .node(labels=labels, ref_name=node_name, properties=properties)
@@ -20,7 +20,7 @@ def get_all_nodes_by_label_and_properties(labels: List[str] | str, properties: D
             )
 
 
-def get_all_paths(src_node_labels: List[str] | str, dst_node_labels: List[str] | str, relationship_type: str, path_name: str = 'p'):
+def get_all_paths(src_node_labels: Union[List[str], str], dst_node_labels: Union[List[str], str], relationship_type: str, path_name: str = 'p'):
     return (QueryBuilder()
             .match()
             .operator_start(operator='', ref_name=path_name)
@@ -32,7 +32,7 @@ def get_all_paths(src_node_labels: List[str] | str, dst_node_labels: List[str] |
             )
 
 
-def get_all_nodes_related_to_nodes(src_node_labels: List[str] | str, dst_node_labels: List[str] | str, relationship_type: str, dst_node_name: str = 'd'):
+def get_all_nodes_related_to_nodes(src_node_labels: Union[List[str], str], dst_node_labels: Union[List[str], str], relationship_type: str, dst_node_name: str = 'd'):
     return (QueryBuilder()
             .match()
             .node(labels=src_node_labels)
@@ -42,7 +42,7 @@ def get_all_nodes_related_to_nodes(src_node_labels: List[str] | str, dst_node_la
             )
 
 
-def get_all_nodes_related_by_fixed_num_of_hops(src_node_labels: List[str] | str, dst_node_labels: List[str] | str, num_hops: int, dst_node_name: str = 'd'):
+def get_all_nodes_related_by_fixed_num_of_hops(src_node_labels: Union[List[str], str], dst_node_labels: Union[List[str], str], num_hops: int, dst_node_name: str = 'd'):
     return (QueryBuilder()
             .match()
             .node(labels=src_node_labels)
@@ -52,7 +52,7 @@ def get_all_nodes_related_by_fixed_num_of_hops(src_node_labels: List[str] | str,
             )
 
 
-def get_all_nodes_related_by_varying_num_of_hops(src_node_labels: List[str] | str, dst_node_labels: List[str] | str, min_hops: int, max_hops: int, dst_node_name: str = 'd'):
+def get_all_nodes_related_by_varying_num_of_hops(src_node_labels: Union[List[str], str], dst_node_labels: Union[List[str], str], min_hops: int, max_hops: int, dst_node_name: str = 'd'):
     return (QueryBuilder()
             .match()
             .node(labels=src_node_labels)
@@ -62,15 +62,15 @@ def get_all_nodes_related_by_varying_num_of_hops(src_node_labels: List[str] | st
             )
 
 
-def get_nodes_with_pagination(node_labels: List[str] | str, node_name: str = 'n', offset: int = 0, limit: int = -1):
+def get_nodes_with_pagination(node_labels: Union[List[str], str], node_name: str = 'n', offset: int = 0, limit: int = -1):
     raise NotImplementedError()
 
 
-def create_node(node_labels: List[str] | str):
+def create_node(node_labels: Union[List[str], str]):
     raise NotImplementedError()
 
 
-def merge_node(node_labels: List[str] | str, properties: Dict[str, Any] = None):
+def merge_node(node_labels: Union[List[str], str], properties: Dict[str, Any] = None):
     return (QueryBuilder()
             .merge()
             .node(labels=node_labels, properties=properties)
@@ -81,8 +81,8 @@ def create_relationship(relationship_type: str):
     raise NotImplementedError()
 
 
-def merge_relationship(relationship_type: str, properties: Dict[str, Any] = None, src_node_labels: List[str] | str = None,
-                        dst_node_labels: List[str] | str = None, src_node_properties: Dict[str, Any] = None, dst_node_properties: Dict[str, Any] = None):
+def merge_relationship(relationship_type: str, properties: Dict[str, Any] = None, src_node_labels: Union[List[str], str] = None,
+                        dst_node_labels: Union[List[str], str] = None, src_node_properties: Dict[str, Any] = None, dst_node_properties: Dict[str, Any] = None):
     return (QueryBuilder()
             .match()
             .node(labels=src_node_labels, properties=src_node_properties, ref_name='src')
